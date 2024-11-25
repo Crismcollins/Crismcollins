da3e5e5a23912a8c30e6ab0c6a4567dea36d1557
crismcollins <cris.m.collins@gmail.com>
2024-11-25 18:34:40 -0300

    Deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import NintendoSwitch from "@/components/NintendoSwitch";
 
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_BACKEND_URL)
   return (
     <NintendoSwitch />
   );
